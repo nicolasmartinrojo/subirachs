addf6aecd25f874c89a42baf7a965408b9719e0d
Nicolas Rojo <nicolas.martin.rojo@gmail.com>
2020-10-11 01:56:30 -0300

    Updates

---
+++ precache-manifest.9365557abac83a73fc92c0544ff30a43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7067edd3cfa93aa4267f89ffe703c4b7",
+    "url": "/subirachs/index.html"
+  },
+  {
+    "revision": "8859db5d9aa105b5c602",
+    "url": "/subirachs/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dc029089536f16659b13",
+    "url": "/subirachs/static/js/2.4fd4d281.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/subirachs/static/js/2.4fd4d281.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8859db5d9aa105b5c602",
+    "url": "/subirachs/static/js/main.6f45aa80.chunk.js"
+  },
+  {
+    "revision": "e19fa2225b4a7406a662",
+    "url": "/subirachs/static/js/runtime-main.fa6a7148.js"
+  }
+]);
